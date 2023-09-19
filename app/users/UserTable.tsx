28158d3e8d710e423d239beb52b68e1339b8843e
Florian Blaschke <florianblaschke@web.de>
2023-09-19 20:30:59 +0200

    first changes, still trying stuff

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,32 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td className="text-black">{user.name}</td>
+            <td className="text-black">{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import UserTable from "./UserTable";
+
+const UsersPage = async () => {
+  return (
+    <>
+      <h1>Users</h1>
+      <UserTable />
+    </>
+  );
+};
+
+export default UsersPage;

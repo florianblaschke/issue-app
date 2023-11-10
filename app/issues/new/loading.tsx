ed4ced670d4b8f47e68807aab898314294e0f143
Florian Blaschke <florianblaschke@web.de>
2023-11-10 18:03:16 +0100

    improved loading screens

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,12 +1,3 @@
-import { Box } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-export default function LoadingNewIssuePage() {
-  return (
-    <Box>
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-}
+export default IssueFormSkeleton;

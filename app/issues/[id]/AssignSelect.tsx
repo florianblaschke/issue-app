dfb6675375b0c8dce3bc71aacbd6cbb56cd311fc
Florian Blaschke <florianblaschke@web.de>
2023-11-18 09:56:53 +0100

    filter issues

--- app/issues/[id]/AssignSelect.tsx
+++ app/issues/[id]/AssignSelect.tsx
@@ -1,42 +1,53 @@
 "use client";
+import Skeleton from "@/app/components/Skeleton";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
-import Skeleton from "@/app/components/Skeleton";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function AssignSelect({ issue }: { issue: Issue }) {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
+  const { data: users, error, isLoading } = useUsers();
+  if (isLoading) return <Skeleton height={25} />;
+  if (error) return null;
+
+  function assignIssue(userId: string) {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedTo: userId || null,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  }
+  return (
+    <>
+      <Select.Root
+        defaultValue={issue.assignedTo || ""}
+        onValueChange={assignIssue}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
+  );
+}
+
+const useUsers = () =>
+  useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users/").then((res) => res.data),
     staleTime: 60 * 1000,
     retry: 3,
   });
-  if (isLoading) return <Skeleton height={25} />;
-  if (error) return null;
-  return (
-    <Select.Root
-      defaultValue={issue.assignedTo || ""}
-      onValueChange={(userId) => {
-        axios.patch("/api/issues/" + issue.id, { assignedTo: userId || null });
-      }}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-  );
-}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,9 +3,20 @@ import { Table } from "@radix-ui/themes";
 import IssueActions from "../components/IssueActions";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import Link from "../components/Link";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: { status: Status };
+}) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
   return (
     <div>
       <IssueActions />
